ccf90b022ea57adf5e6da1e345938a257a54f8c4
Valentin Stieger <stiegervalentin123@gmail.com>
2024-03-13 13:44:48 -0300

    Initial commit

---
+++ src/main/java/valentin/primerrepositorio/Primerrepositorio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package valentin.primerrepositorio;
+
+/**
+ *
+ * @author Gose is Lose
+ */
+public class Primerrepositorio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
